3ab656cac0f23681710fd46aed7981820ad2b59e
Narcisse <cabrelfouegap09@gmail.com>
2021-04-05 20:12:20 +0100

    cv_responsive

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let x = prompt("entrez la valeur de x");
+x=parseInt(x);
+let y = prompt("entrez la valeur de y");
+y=parseInt(y);
+x = x + y;
+y = x - y;
+x = y - x;
+alert("la nouvelle valeur de x est: " +x +"la nouvelle valeur de y est : "+y);
+
